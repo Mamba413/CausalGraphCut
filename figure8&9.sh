ba5bff12e189dd380ea4cb32a9155e5bfd9ca03b
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2025-05-26 10:41:00 +0800

    Update and rename figure8.sh to figure8&9.sh

---
+++ figure8&9.sh
@@ -0,0 +1,45 @@
+PYTHON_PATH="/opt/anaconda3/bin/python"  ## your python path
+
+cor_types=("example1" "example2" "example3")
+pattern_types=("hexagon")
+nums=(20 40 60 80 100 120 140)
+
+SCRIPT_PATH="/CausalGraphCut/sim_ss.py"
+for pattern in "${pattern_types[@]}"; do
+    case $pattern in
+        "hexagon")
+            grid=12
+            ;;
+        # Add more patterns with corresponding grid sizes here
+        *)
+            echo "Unknown pattern: $pattern"
+            continue
+            ;;
+    esac
+    for cor_type in "${cor_types[@]}"; do
+        for num in "${nums[@]}"; do
+            echo "Running simulation with sample-num=$num"
+            $PYTHON_PATH $SCRIPT_PATH --pattern="$pattern" --rho=0.5 --cor-type="$cor_type" --sample-num=$num --grid-size=$grid
+        done
+    done
+done
+
+SCRIPT_PATH="/CausalGraphCut/sim_td.py"
+for pattern in "${pattern_types[@]}"; do
+    case $pattern in
+        "hexagon")
+            grid=12
+            ;;
+        # Add more patterns with corresponding grid sizes here
+        *)
+            echo "Unknown pattern: $pattern"
+            continue
+            ;;
+    esac
+    for cor_type in "${cor_types[@]}"; do
+        for num in "${nums[@]}"; do
+            echo "Running simulation with sample-num=$num"
+            $PYTHON_PATH $SCRIPT_PATH --pattern="$pattern" --rho=0.5 --cor-type="$cor_type" --sample-num=$num --grid-size=$grid
+        done
+    done
+done
